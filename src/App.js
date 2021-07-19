f72c76a980625a0730421cf15c87641caa8fe75d
Dakota Nelson <kotalilyy@gmail.com>
2021-07-18 21:41:46 -0500

    create login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Login from './components/Login/Login';
 import Preferences from "./components/Preferences/Preferences";
 
 function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
