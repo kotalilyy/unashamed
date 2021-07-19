ca7849d8731d7d0662d92ee07fc06e11312a1bfc
Dakota Nelson <kotalilyy@gmail.com>
2021-07-18 21:09:42 -0500

    import components, create routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Preferences from "./components/Preferences/Preferences";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/main.js
+++ src/components/main.js
@@ -4,4 +4,3 @@ const chk = document.getElementById("chk");
 chk.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
-
