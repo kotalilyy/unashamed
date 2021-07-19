55d4281bb512ced32bd69e29b4018e1dde54a3fd
Dakota Nelson <kotalilyy@gmail.com>
2021-07-18 22:55:40 -0500

    create local state to capture username & password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,33 @@
-import './Login.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-export default function Login() {
-  return(
+import "./Login.css";
+
+export default function Login({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+  return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={(e) => setUserName(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
